0fa6acbe16cdd0d5c14822aa6f4ff0864dbadf79
Petr Abdulin <pabdulin@gmail.com>
2024-05-01 12:54:19 +0700

    Vector operations add, sub, mul, div

--- src/vector.c
+++ src/vector.c
@@ -1,6 +1,73 @@
 #include <math.h>
 #include "vector.h"
 
+float vec2_lenght(vec2_t v) {
+    return sqrtf(v.x*v.x + v.y*v.y);
+}
+
+vec2_t vec2_add(vec2_t a, vec2_t b) {
+    vec2_t result;
+    result.x = a.x + b.x;
+    result.y = a.y + b.y;
+    return result;
+}
+
+vec2_t vec2_sub(vec2_t a, vec2_t b) {
+    vec2_t result;
+    result.x = a.x - b.x;
+    result.y = a.y - b.y;
+    return result;
+}
+
+vec2_t vec2_mul(vec2_t v, float factor) {
+    vec2_t result = {
+        .x = v.x * factor,
+        .y = v.y * factor
+    };
+    return result;
+}
+
+vec2_t vec2_div(vec2_t v, float factor) {
+    float div_factor = 1.0f / factor;
+    vec2_t result = vec2_mul(v, div_factor);
+    return result;
+}
+
+float vec3_lenght(vec3_t v) {
+    return sqrtf(v.x*v.x + v.y*v.y + v.z*v.z);
+}
+
+vec3_t vec3_add(vec3_t a, vec3_t b) {
+    vec3_t result;
+    result.x = a.x + b.x;
+    result.y = a.y + b.y;
+    result.y = a.z + b.z;
+    return result;
+}
+
+vec3_t vec3_sub(vec3_t a, vec3_t b) {
+    vec3_t result;
+    result.x = a.x - b.x;
+    result.y = a.y - b.y;
+    result.y = a.z - b.z;
+    return result;
+}
+
+vec3_t vec3_mul(vec3_t v, float factor) {
+    vec3_t result = {
+        .x = v.x * factor,
+        .y = v.y * factor,
+        .z = v.z * factor
+    };
+    return result;
+}
+
+vec3_t vec3_div(vec3_t v, float factor) {
+    float div_factor = 1.0f / factor;
+    vec3_t result = vec3_mul(v, div_factor);
+    return result;
+}
+
 vec3_t vec3_rotate_x(vec3_t v, float angle) {
     vec3_t rotated_vector = {
         .x = v.x,
